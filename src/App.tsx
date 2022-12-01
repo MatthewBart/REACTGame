9a57a7748bb3f75fbda7f48dd6f3114d488830a8
MatthewBart <MatthewCBartley@gmail.com>
2022-12-01 14:17:59 +0000

    Functioning game with set win conditions. Restart game and randomised game array still needed to be added

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,68 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// New type for handling current and previous clicked cards
+type TLocation={
+  row: number;
+  col: number;
+}
 function App() {
   // Define 2d Array to use as the board
   const [boardGrid, setBoardGrid] = useState([
     [6,1,2,3],[4,5,6,1],[2,3,4,5]
   ]);
+  
+  // Define constant for last clicked card of type TLocation
+  const [lastClicked, setLastClicked]=useState<TLocation | undefined>();
 
   // Copies the 2d array and fills it with false
-  // Used to check if 2 cards match
-  // These are then set to true if they match thus eliminating them from being used again
+  // Used for setting cards as flipped on the board
   const [flippedGrid, setFlippedGrid] = useState(
     new Array(boardGrid.length).fill("").map(()=>new Array(boardGrid[0].length).fill(false))
   );
   
-  // Handles selected card
-  // reveals card by creating a new flipped grid and setting the card index to true
+  // Handles selected card and last selected card
+  // Compares cards and checks if they match or not
+  // Reveals card by creating a new flipped grid and setting the card index to true
+  // If the cards do not match, flip them over again after a second
+  // If it is a match leave them face up and check if all cards are matched
   function handleCardSelect(rowIndex: number, colIndex: number): void {
-    const newFlippedGrid=[...flippedGrid];
+    const currentCard = boardGrid[rowIndex][colIndex];
+    const newFlippedGrid =[...flippedGrid];
     newFlippedGrid[rowIndex][colIndex]=true;
     setFlippedGrid(newFlippedGrid);
+    // if a card has been selected prior then compare them
+    if(lastClicked){
+      const previousCard = boardGrid[lastClicked.row][lastClicked.col];
+      // second card selected
+      // check match
+      if(previousCard !==currentCard)
+      {
+        // wait 1.5 seconds and hide both flipped cards
+          setTimeout(()=>{
+            newFlippedGrid[rowIndex][colIndex]=false;
+            newFlippedGrid[lastClicked.row][lastClicked.col]=false;
+            setFlippedGrid([...newFlippedGrid]);
+          },1500);
+      }
+      // check win conditions
+      // check if all elements are true
+      else{
+        const win = flippedGrid.flat().every(element => element ===true);
+        if(win){alert('Winner!');}
+        
+      }
+    setLastClicked(undefined);
+    }else{
+      // first card selected
+      setLastClicked({
+        row: rowIndex,
+        col: colIndex,
+    })
+
+
+    }
   }
-  console.log("flippedGrid",flippedGrid);
   
   return (
     // Displays the boardGrid array in a single column using mapping
